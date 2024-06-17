7809e4a192fc90e9e6042bd2e15ad3cd5ef4bfac
WillSpector <Dersonwill@gmail.com>
2024-06-17 17:27:54 +0300

    1. Added methods space() and dot() in class Concatenation

--- src/main/java/аbstract/Concatenation.java
+++ src/main/java/аbstract/Concatenation.java
@@ -5,5 +5,15 @@ public class Concatenation {
     public void and() {
         System.out.print(" and ");
     }
+
+    // Метод выводит на экран "пробел"
+    public void space() {
+        System.out.println(" ");
+    }
+
+    // Метод выводит на экран точку
+    public void dot() {
+        System.out.println(".");
+    }
 }
 
